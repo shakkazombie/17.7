d7f53e646bc7c79373d593d6dce8ea014013a9fa
Lukasz Babicki <lukaszbabicki@gmail.com>
2018-07-19 10:10:08 +0200

    add end task

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '867634279143-qmm6o5vj3kv6k990at5cnvonad3as9vm.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': '-VJm8n6mNJ071FUFG79-ycMo',
+    'CALLBACK_URL':'http://localhost:3000/auth/google/callback'
+};
